6f48f9894b34b02c8fe9ceca92c67009622309d5
AchanthiSundar <tnrdmobile@gmail.com>
2021-03-15 15:48:14 +0530

    log_in screen  design changes

--- app/src/main/java/com/nic/TPTaxDepartment/activity/SplashScreen.java
+++ app/src/main/java/com/nic/TPTaxDepartment/activity/SplashScreen.java
@@ -51,7 +51,7 @@ public class SplashScreen extends AppCompatActivity implements
 //                showSignInScreen();
 
             }
-        } else {checkAppVersion();
+        } else {
 //            showSignInScreen();
         }
 
